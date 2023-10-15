a42f48a6c5c43abe1953970d8658555c560e3dbc
matttiepie <124634024+matttiepie@users.noreply.github.com>
2023-10-14 23:05:41 -0500

    Add files via upload

---
+++ checkout.js
@@ -0,0 +1,30 @@
+/*var myCheckout = {
+    // Properties
+    
+        this: item='',
+        this: price=0,
+        this: quantity=0,
+    
+    
+    // Methods
+    calculateTotal: function() {
+        var total=this.price*this.quantity;
+        document.write("You owe this much: " + total);
+    }
+};*/
+function showMessage(message){
+    alert("you clicked the button: " + message);
+}
+const myButton=document.getElementById("button1");
+
+myButton.addEventListener("click",function()
+{
+    
+    showMessage("hello, world!");
+    <form method="post" action="connect.php"></form>
+    
+    
+    
+});
+
+
